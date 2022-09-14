ef06a5f1262619a36274a2104db515c5506b1b6d
Enzoro <enzocantoni03@gmail.com>
2022-09-14 09:58:37 -0300

    - InvManager + Comentada y arregladas Elfo y Enano

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -4,42 +4,36 @@ namespace Roleplay
 {
     public class Elfo
     {
+        // Atributos privados nombre, vida, ataque, defensa, inventario y si está vivo el personaje
         private string nombre;
         private float vida;
         private int ataque;
         private int defensa;
         private bool isVivo;
+        private int inicialVida = 100;
+        private int inicialAtaque = 1;
+        private int inicialDefensa = 1;
 
+        // Constructor: Solo le pasa el nombre en la creación de personaje
+        // Seteamos la vida, ataque y defensa del elfo a sus variables iniciales
+        // isVivo true, porque el personaje aparece vivo.
         public Elfo(String Nombre)
         {
             this.nombre = Nombre;
-            this.vida = 100;
-            this.ataque = 1;
-            this.defensa = 1;
+            this.vida = inicialVida;
+            this.ataque = inicialAtaque;
+            this.defensa = inicialDefensa;
             this.isVivo = true;
 
             /*this.objetos = Objetos[];*/
         }
+
+        // Atributos publicos del Elfo. Acceder y Modificar sus atributos privados.
         public string Nombre {get;}
         public float Vida {get; set;}
-        public int Ataque {get;}
-        public int Defensa {get;}
+        public int Ataque {get; set;}
+        public int Defensa {get; set;}
         public bool IsVivo {get; set;}
 
-        // public void AtaqueEnano(Enano Object)
-        // {
-        //     if Object.isVivo()
-        //     {
-
-        //     }
-        // }
-
-        // public void AtaqueElfo(Enano Object)
-        // {
-        //     if Object.isVivo()
-        //     {
-
-        //     }
-        // }
     }
 }
--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -1,35 +1,44 @@
 ﻿using System;
-namespace Roleplay{
+namespace Roleplay
+{
 
     public class Enano
     {
-        //Constructor  -->
+        // Propiedades Comunes de los tipos de personajes. Cambian los valores de Defensa y Vida
+        // por ser enanos. Son más duritos
 
         private string nombre;
         private float vida;
         private int ataque;
         private int defensa;
         private bool isVivo;
+        private int inicialVida = 300;
+        private int inicialAtaque = 1;
+        private int inicialDefensa = 5;
 
+        //Constructor como el de Elfo. 
         public Enano(String Nombre)
         {
             this.nombre = Nombre;
-            this.vida = Vida;
-            this.ataque = Ataque;
-            this.defensa = Defensa;
+            this.vida = inicialVida;
+            this.ataque = inicialAtaque;
+            this.defensa = inicialDefensa;
             this.isVivo = true;
 
             /*this.objetos = Objetos[];*/
         }
+
+        // Atributos publicos para acceder y poder modificar los atributos privados del Enano
         public string Nombre {get;}
         public float Vida {get; set;}
         public int Ataque {get;}
         public int Defensa {get;}
         public bool IsVivo {get; set;}
 
+        // Ataques a distintas clases del Enano al resto
         public void AtaqueElfo(Elfo other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -38,13 +47,13 @@ namespace Roleplay{
             other.Vida -= this.ataque;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
 
         public void AtaqueMago(Mago other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -53,13 +62,13 @@ namespace Roleplay{
             other.Vida -= this.ataque;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
 
         public void AtaqueEnano(Enano other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -68,13 +77,16 @@ namespace Roleplay{
             other.Vida -= this.ataque;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
 
+
+        // AtaqueFuerte es una habilidad especial del Enano
+        // Consiste en un ataque randomizado que puede o pegar más, o pegar menos que un ataque normal
         public void AtaqueFuerteMago(Mago other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -85,13 +97,13 @@ namespace Roleplay{
             other.Vida -= this.ataque * Critico;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
 
         public void AtaqueFuerteElfo(Elfo other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -102,13 +114,13 @@ namespace Roleplay{
             other.Vida -= this.ataque * Critico;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
 
         public void AtaqueFuerteEnano(Enano other)
         {
-            if (!(other.isVivo)) 
+            if (!(other.IsVivo)) 
             {
                 Console.WriteLine("Dejalo, ya está muerto");
                 return;
@@ -119,7 +131,7 @@ namespace Roleplay{
             other.Vida -= this.ataque * Critico;
             if (other.Vida <= 0)
             {
-                other.isVivo = false;
+                other.IsVivo = false;
             }
         }
     }
