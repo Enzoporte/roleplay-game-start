8107543044fac7387b7941a96453fccc027ed173
Gonzalo <87908978+Gonza50@users.noreply.github.com>
2022-09-14 08:29:20 -0300

    Clase elfo sin terminar

---
+++ src/Library/Elfos.cs
@@ -0,0 +1,35 @@
+namespace Rolplay
+{
+    public class Elfos
+    {
+        public string Nombre;
+        public int Vida;
+        public int Ataque;
+        public int Defensa;
+        // public Elemento[];
+
+        public Elfos(string nombre)
+        {
+            this.Nombre = nombre;
+            this.Vida = 100;
+            this.Ataque = 0;
+            this.Defensa = 0;
+        }
+
+        // public void AtaqueEnano(Enano Object)
+        // {
+        //     if Object.isVivo()
+        //     {
+
+        //     }
+        // }
+
+        // public void AtaqueElfo(Enano Object)
+        // {
+        //     if Object.isVivo()
+        //     {
+
+        //     }
+        // }
+    }
+}
