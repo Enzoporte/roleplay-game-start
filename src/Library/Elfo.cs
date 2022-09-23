46a209dbd9e570af2d69bd598e7d834cc44e7988
Enzoporte <enzocantoni03@gmail.com>
2022-09-23 15:34:06 -0300

    Actualizacion de clases (- mago). Agregue interfac

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -2,72 +2,74 @@ using System;
 
 namespace Roleplay
 {
-    public class Elfo
+    public class Elfo : Personaje, IAtaqueHabilidad
     {
         // Atributos privados nombre, vida, ataque, defensa, inventario y si está vivo el personaje
         private string nombre;
         private float vida;
-        private int ataque;
+        private int daño;
         private int defensa;
         private bool isVivo;
-        private int inicialVida = 100;
-        private int inicialAtaque = 1;
+        private int inicialVida = 100; //También su vida Máxima inicial. Se podría crear otro atributo para vida máxima y que pueda subir
+        private int inicialDaño = 1;
         private int inicialDefensa = 1;
+        private int poderCuracion = 10;
 
         // Constructor: Solo le pasa el nombre en la creación de personaje
         // Seteamos la vida, ataque y defensa del elfo a sus variables iniciales
         // isVivo true, porque el personaje aparece vivo.
-        public Elfo(String Nombre)
+        public Elfo(String nombre) : base(nombre)
         {
-            this.nombre = Nombre;
+            this.nombre = nombre;
             this.vida = inicialVida;
-            this.ataque = inicialAtaque;
+            this.daño = inicialDaño;
             this.defensa = inicialDefensa;
             this.isVivo = true;
-
-            /*this.objetos = Objetos[];*/
+            // Inventario
         }
 
-        // Atributos publicos del Elfo. Acceder y Modificar sus atributos privados.
-        public string Nombre {get;}
-        public float Vida {get; set;}
-        public int Ataque {get; set;}
-        public int Defensa {get; set;}
-        public bool IsVivo {get; set;}
-
-        public void AtaqueEnano(Enano other)
+        public void Atacar(Personaje other)
         {
-            if (other.isVivo) {
-                other.vida -= this.Ataque;
-                if (other.vida >= 0)
-                {
-                    other.isVivo = false;
-                }
+            if (!(other.IsVivo)) 
+            {
+                Console.WriteLine("Dejalo, ya está muerto");
+                return;
             }
-        }
 
-        public void AtaqueElfo(Elfo other)
+            if (other.Defensa < this.Daño) 
+            {
+                other.Vida -= (this.Daño - other.Defensa);
+                if (other.Vida <= 0)
+                {
+                    other.IsVivo = false;
+                }
+                Console.WriteLine($"{other.Nombre} ha muerto!");
+                return;
+            }
+            other.Vida--;
+        }
+        
+        public void UsarHabilidad(Personaje other)
         {
-            if (other.isVivo) {
-                other.vida -= this.Ataque;
-                if (other.vida >= 0)
-                {
-                    other.isVivo = false;
-                }
-            }
+            // --- Curar ---
+           if (!(other.IsVivo))
+           {
+                Console.WriteLine($"{other.Nombre} está muerto, no puedes curarlo.");
+                return;
+           }
+
+           if (other.Vida >= other.InicialVida)
+           {
+                Console.WriteLine($"{other.Nombre} ya tiene su vida al máximo");
+                return;
+           }
+
+           other.Vida += poderCuracion;
+           if (other.Vida > other.InicialVida)
+           {
+                other.Vida = InicialVida;
+           }
+
         }
-
-        public void AtaqueMago(Mago other)
-        {
-            if (other.isVivo) {
-                other.vida -= this.Ataque;
-                if (other.vida >= 0)
-                {
-                    other.isVivo = false;
-                }
-            }
-        }
-
-
     }
 }
--- src/Library/LibroDeHechizos.cs
+++ src/Library/LibroDeHechizos.cs
@@ -1,12 +1,19 @@
 namespace Roleplay
 {
-    public class LibroDeHechizos
+    public class LibroDeHechizos : Elemento
     {
         private string descripcion;
+        private int ataque;
+        private int defensa;
         private Hechizo[] hechizos;
 
-        public string Descripcion {get;}
-        public Hechizo[] Hechizos {get;}
+        public LibroDeHechizos(string descripcion, int ataque, int defensa) : base(descripcion, ataque, defensa)
+        {
+            this.descripcion = descripcion;
+            this.ataque = ataque;
+            this.defensa = defensa;
+        }
+        public Hechizo[] Hechizos {get; set;}
         
 
     }
